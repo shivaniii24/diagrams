b1ab376c9e846223cf505270ff80474b0c616b7c
shivaniii24 <64369944+shivaniii24@users.noreply.github.com>
2024-03-05 12:36:53 +0530

    Update __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,11 @@
+"""
+resources.py provides useful tools for resources processing.
+
+There are 2 commands available.
+- clean: clean and unify the resources file names with some rules.
+- round: generate the rounded images from the original squared images.
+"""
+
+import os
+import subprocess
+import sys
